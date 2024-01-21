50110674ea6f7536326ab7e7aac6b40e6397c247
kevindgzmn <kevindgzmn@gmail.com>
2024-01-21 14:55:42 +0000

    Added a sample json response string in generate component and removed all mat-actions from all components that uses mat-card

--- src/app/personas-generation/personas-generation.component.ts
+++ src/app/personas-generation/personas-generation.component.ts
@@ -6,5 +6,14 @@ import { Component } from '@angular/core';
   styleUrl: './personas-generation.component.scss'
 })
 export class PersonasGenerationComponent {
+  jsonResponse: string;
 
+  constructor() {
+    this.jsonResponse = "{ \"Diogo Buarque Franzosi - Data\": { \"Data Scientist\": 0.3457283675670624, \"Data Scientist in Contamination\": 0.3601120114326477,"
+      + "\"Data Scientist- Pricing insights\": 0.3513360619544983, \"Senior Data Analyst\": 0.36060917377471924, \"Senior Data scientist with excellent software development skills\": 0.3502436578273773 } }";
+  }
+
+  ngOninit() {
+
+  }
 }
